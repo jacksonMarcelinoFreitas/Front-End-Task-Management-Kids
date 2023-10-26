e8cb19ab4a13324cfcef5715bd5652cec446b286
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-10-26 20:00:50 -0300

    :rocket:

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: "http://34.228.223.216:8080"
+  baseURL: "https://34.228.223.216:8080"
   // baseURL: "http://localhost:8080"
 });
 
