884251c1368480d16ccc5175042ca270b7e99619
Jackson Freitas <jacksonzitap.mc@gmail.com>
2023-10-30 19:44:12 -0300

    :wrench: test deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import { AuthProvider } from './hooks/auth'; //hook de autenticação
 import GlobalStyles from './styles/global';
 import theme from './styles/theme.ts';
 
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Routes } from './routes';
 import { ToastContainer } from 'react-toastify';
-
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-  import { defineConfig } from 'vite'
-  import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  // https://vitejs.dev/config/
-  export default defineConfig({
-    plugins: [react()],
-    base: "/Front-End-Task-Management-Kids/",
-    define: {
-      'process.env': process.env,
-    },
-  })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: "/vite-deploy/"
+})
